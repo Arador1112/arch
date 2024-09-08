9a93d9657d5c2683fa40c40c3f3acb384df1a8b3
Arador1112 <chaitanyachandu523@gmail.com>
2024-09-08 23:09:11 +0530

    arch hypr nvim

---
+++ nvim/lua/arador/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  { 
+
+    "catppuccin/nvim", 
+    name = "catppuccin", 
+    priority = 1000,
+    config = function()
+    require("catppuccin").setup({
+    flavour = "frappe",
+
+
+  })
+      -- load the colorscheme here
+      vim.cmd([[colorscheme catppuccin]])
+    end,
+  },
+}
