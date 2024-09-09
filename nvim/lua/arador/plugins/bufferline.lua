00261061580b401292488212b23715f5e0e9148e
Arador1112 <chaitanyachandu523@gmail.com>
2024-09-10 01:26:16 +0530

    updated nvim

--- nvim/lua/arador/plugins/bufferline.lua
+++ nvim/lua/arador/plugins/bufferline.lua
@@ -1,11 +1,22 @@
 return {
-  "akinsho/bufferline.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  version = "*",
-  opts = {
-    options = {
-      mode = "tabs",
-      separator_style = "slant",
-    },
-  },
+	"akinsho/bufferline.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	version = "*",
+	opts = {
+		options = {
+			mode = "tabs",
+			separator_style = "thin",
+			color_icons = false,
+			show_buffer_close_icons = false,
+			show_close_icon = false,
+			offsets = {
+				{
+					filetype = "NvimTree",
+					text = "NvimTree",
+					text_align = "left",
+					separator = false,
+				},
+			},
+		},
+	},
 }
--- nvim/lua/arador/plugins/colorscheme.lua
+++ nvim/lua/arador/plugins/colorscheme.lua
@@ -1,17 +1,62 @@
 return {
-  { 
 
-    "catppuccin/nvim", 
-    name = "catppuccin", 
-    priority = 1000,
-    config = function()
-    require("catppuccin").setup({
-    flavour = "frappe",
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "frappe",
+				-- Uncomment the line below to apply the colorscheme
+				vim.cmd("colorscheme catppuccin-frappe"),
+			})
+		end,
+	},
+	{
+		"akinsho/bufferline.nvim",
+		after = "catppuccin", -- Ensure bufferline loads after catppuccin
+		config = function()
+			local palette = require("catppuccin.palettes").get_palette("frappe")
 
+			require("bufferline").setup({
+				options = {
+					mode = "tabs",
+					separator_style = "thin",
+					offsets = {
+						{
+							filetype = "NvimTree",
+							text = "NvimTree",
+							text_align = "left",
+							separator = false,
+						},
+					},
+					color_icons = false,
+					show_buffer_close_icons = false,
+					show_close_icon = false,
+				},
+				highlights = {
+					fill = {
+						bg = palette.base,
+						fg = palette.text,
+					},
+					buffer_selected = {
+						bold = true,
+					},
+				},
+			})
+		end,
+	},
 
-  })
-      -- load the colorscheme here
-      vim.cmd([[colorscheme catppuccin]])
-    end,
-  },
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		priority = 1000,
+		config = function()
+			require("rose-pine").setup({
+				flavour = "moon",
+				-- Uncomment the line below to apply the colorscheme
+				-- vim.cmd("colorscheme rose-pine"),
+			})
+		end,
+	},
 }
